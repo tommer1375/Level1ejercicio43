c2e65ea0b394d71ced6f58c5b9acd3769e83d571
Jorge Monzó <151056618+tommer1375@users.noreply.github.com>
2024-02-05 13:50:47 +0100

    Update n4ejercicio3.java

--- src/n4ejercicio3.java
+++ src/n4ejercicio3.java
@@ -1,29 +1,12 @@
-package n4ejercicio3;
+public class Main {
 
-import java.sql.Array;
-import java.util.*;
-
-public class n4ejercicio3 {
     public static void main(String[] args) {
 
-       Scanner myObj = new Scanner(System.in);
-        int[] array = {1, 2, 3, 4, 5, 6,};
 
-        try {
+        ArrayIndexOutOfBoundsExceptionClass exceptionClass = new ArrayIndexOutOfBoundsExceptionClass();
+        exceptionClass.throwArrayIndexOutOfBoundsException();
 
-
-            System.out.println("Indica una posición para saber si existe en el array");
-            int posicion=myObj.nextInt();
-
-            System.out.println(array[posicion]);
-
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Error: número fuerda de indice" + e);
-        } finally {
-            System.out.println("Fin del programa");
-        }
     }
 
 }
-
 

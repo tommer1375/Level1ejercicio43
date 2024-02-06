83bbb6e1ad23e6c07361e9320638947e4bb59a71
Jorge Monzó <151056618+tommer1375@users.noreply.github.com>
2024-02-06 11:20:23 +0100

    Update ArrayIndexOutOfBoundsExceptionClass.java

--- src/ArrayIndexOutOfBoundsExceptionClass.java
+++ src/ArrayIndexOutOfBoundsExceptionClass.java
@@ -1,9 +1,1 @@
-public class ArrayIndexOutOfBoundsExceptionClass {
 
-    private int[] array = new int[10];
-
-    public void throwArrayIndexOutOfBoundsException() {
-        array[11] = 1;
-    }
-
-}
